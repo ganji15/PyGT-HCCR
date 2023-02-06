37c4dd47b4d8bf6ebb4ba47bf45ea6118eab817b
ganji <ganji15@users.noreply.github.com>
2023-02-06 09:53:23 +0800

    Update boost_off_hccr_train.sh

--- PyGT/scripts/boost_off_hccr_train.sh
+++ PyGT/scripts/boost_off_hccr_train.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=0,1  OMP_NUM_THREADS=1  torchrun --nproc_per_node=2 dist_train.py --net-id pgt_l --epochs 6 --dataset gen_off_hccr --save-ckpt ckpt/boost_pretrain --workers 4  --batch-size 256  --first-decay 0.7 --last-decay 0.9 --lr 0.002 --in-features 3 --save-iter 2500 --online 0
+CUDA_VISIBLE_DEVICES=0,1  OMP_NUM_THREADS=1  torchrun --nproc_per_node=2 dist_train.py --net-id pgt_l --epochs 10 --dataset gen_off_hccr --save-ckpt ckpt/boost_pretrain --workers 4  --batch-size 256  --first-decay 0.7 --last-decay 0.9 --lr 0.002 --in-features 3 --save-iter 2500 --online 0
 CUDA_VISIBLE_DEVICES=0,1  OMP_NUM_THREADS=1  torchrun --nproc_per_node=2 dist_train.py --net-id pgt_l --epochs 10 --dataset off_hccr  --load-ckpt  ckpt/boost_pretrain/best_0.ckpt --workers 4  --batch-size 256  --first-decay 0.7 --last-decay 0.9 --lr 0.002 --in-features 3 --save-iter 2500 --online 0
 
